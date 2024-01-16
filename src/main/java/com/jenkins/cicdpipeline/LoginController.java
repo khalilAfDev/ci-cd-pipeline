ee65d951d9c9519a85bf0b79561e6a673890ce5a
oadya <44276320+oadya@users.noreply.github.com>
2024-01-16 05:21:58 +0100

    Update LoginController.java

--- src/main/java/com/jenkins/cicdpipeline/LoginController.java
+++ src/main/java/com/jenkins/cicdpipeline/LoginController.java
@@ -13,5 +13,14 @@ public class LoginController {
     public String index(){
         return "index";
     }
+
+    public int task1(int a, int b) {
+    return a + b;
+    }
+
+public int task2(int a, int b) {
+    int sum = a  + b;
+    return sum;
+}
 	
 }
